bcd9b34a2b251caa0d7300df13cd4537fb86fdde
jessica.mathi <jessicamathi95@gmail.com>
2022-11-09 17:07:17 -0500

    testing code

---
+++ Code/Loading Data/loadc.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import pandas as pd
+
+quantquestions=pd.read_csv(r'D:\Python Project\CQuestions.csv',encoding='latin1')
+
+print(quantquestions.head());
+
+cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1',database='swear');
+cursor=cnx.cursor();
+
+for i in range(0,len(quantquestions)):
+    sno = str(quantquestions.at[i,'sno'])
+    question = str(quantquestions.at[i,'question'])
+    option1 = str(quantquestions.at[i,'option1'])
+    option2 = str(quantquestions.at[i,'option2'])
+    option3 = str(quantquestions.at[i,'option3'])
+    option4 = str(quantquestions.at[i,'option4'])
+    ans = str(quantquestions.at[i,'ans'])
+    values = sno+",'"+question+"','"+option1+"','"+option2+"','"+option3+"','"+option4+"','"+ans+"'";
+    print(values)
+    sql_c="insert into cquestions values ("+values+");"
+    cursor.execute(sql_c);
+cnx.commit();
---
+++ Code/Loading Data/loadverbal.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import pandas as pd
+
+verbalquestions=pd.read_csv(r'D:\Python Project\questionsverbal.csv')
+
+print(verbalquestions.head());
+
+cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1',database='swear');
+cursor=cnx.cursor();
+
+for i in range(0,len(verbalquestions)):
+    sno = str(verbalquestions.at[i,'sno'])
+    question = str(verbalquestions.at[i,'question'])
+    option1 = str(verbalquestions.at[i,'option1'])
+    option2 = str(verbalquestions.at[i,'option2'])
+    option3 = str(verbalquestions.at[i,'option3'])
+    option4 = str(verbalquestions.at[i,'option4'])
+    ans = str(verbalquestions.at[i,'ans'])
+    values = sno+",'"+question+"','"+option1+"','"+option2+"','"+option3+"','"+option4+"','"+ans+"'";
+    #print(values)
+    sql_c="insert into verbalquestions values ("+values+");"
+    cursor.execute(sql_c);
+cnx.commit();
